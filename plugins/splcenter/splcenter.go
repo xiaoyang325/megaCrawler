61cb7aeb236d5e1394c6c9a3e83d78accf750701
Mamoruque <wongchunyau@stu2022.jnu.edu.cn>
2022-11-13 14:22:50 +0800

    Add plugin for www.splcenter.org

--- plugins/splcenter/splcenter.go
+++ plugins/splcenter/splcenter.go
@@ -2,18 +2,17 @@ package splcenter
 
 import (
    "github.com/gocolly/colly/v2"
-   "megaCrawler/megaCrawler"
+   "megaCrawler/Crawler"
    "strings"
 )
 
 func init() {
-   w := megaCrawler.Register("splcenter", "南方贫困法律中心", 
-      "https://www.splcenter.org/")
+   w := Crawler.Register("splcenter", "南方贫困法律中心", "https://www.splcenter.org/")
 
    w.SetStartingUrls([]string{
-      "https://www.splcenter.org/features-stories",
-      "https://www.splcenter.org/fighting-hate/extremist-files",
-      "https://www.splcenter.org/hatewatch",
+      "https://www.splcenter.org/features-stories/",
+      "https://www.splcenter.org/fighting-hate/extremist-files/",
+      "https://www.splcenter.org/hatewatch/",
       "https://www.splcenter.org/year-hate-extremism-2021",
       "https://www.splcenter.org/issues/hate-and-extremism",
       "https://www.splcenter.org/issues/childrens-rights",
@@ -26,73 +25,67 @@ func init() {
 
    // 从频道入口访问更多信息的 Index
    w.OnHTML(".more-link a", 
-      func(element *colly.HTMLElement, ctx *megaCrawler.Context) {
-         w.Visit(element.Attr("href"), megaCrawler.News)
+      func(element *colly.HTMLElement, ctx *Crawler.Context) {
+         w.Visit("https://www.splcenter.org" + element.Attr("href"), Crawler.Index)
       })
 
    // 从翻页器获取下一页 Index 并访问
    w.OnHTML(".pager-next > a", 
-      func(element *colly.HTMLElement, ctx *megaCrawler.Context) {
-         w.Visit(element.Attr("href"), megaCrawler.Index)
+      func(element *colly.HTMLElement, ctx *Crawler.Context) {
+         w.Visit("https://www.splcenter.org" + element.Attr("href"), Crawler.Index)
       })
 
    // 从 Index 访问 News
    w.OnHTML(`#main-content div[class="field-item even "] > h1 > a`, 
-      func(element *colly.HTMLElement, ctx *megaCrawler.Context) {
-         w.Visit(element.Attr("href"), megaCrawler.News)
+      func(element *colly.HTMLElement, ctx *Crawler.Context) {
+         w.Visit("https://www.splcenter.org" + element.Attr("href"), Crawler.News)
       })
 
    // 获取 Title
    w.OnHTML(".group-header h1", 
-      func(element *colly.HTMLElement, ctx *megaCrawler.Context) {
+      func(element *colly.HTMLElement, ctx *Crawler.Context) {
          ctx.Title = strings.TrimSpace(element.Text)
       })
 
    // 获取 Publication Time
    w.OnHTML(".date-display-single", 
-      func(element *colly.HTMLElement, ctx *megaCrawler.Context) {
-         ctx.PublicationTime = strings.TrimSpace(element.Text)
-      })
-
-   // 获取 Publication Time
-   w.OnHTML(".date-display-single", 
-      func(element *colly.HTMLElement, ctx *megaCrawler.Context) {
+      func(element *colly.HTMLElement, ctx *Crawler.Context) {
          ctx.PublicationTime = strings.TrimSpace(element.Text)
       })
 
    // 获取 Authors（情况一）
    w.OnHTML(`div[class="field field-name-field-person field-type-entityreference field-label-hidden"] .field-items a`, 
-      func(element *colly.HTMLElement, ctx *megaCrawler.Context) {
+      func(element *colly.HTMLElement, ctx *Crawler.Context) {
          ctx.Authors = append(ctx.Authors, strings.TrimSpace(element.Text))
       })
 
    // 获取 Authors（情况二）
    w.OnHTML(`div[class="field field-name-field-byline field-type-text field-label-hidden"] .field-items div`, 
-      func(element *colly.HTMLElement, ctx *megaCrawler.Context) {
+      func(element *colly.HTMLElement, ctx *Crawler.Context) {
          ctx.Authors = append(ctx.Authors, strings.TrimSpace(element.Text))
       })
 
    // 获取 Authors（情况三）
    w.OnHTML(`div[class="field field-name-title-field field-type-text field-label-hidden"] .field-items a`, 
-      func(element *colly.HTMLElement, ctx *megaCrawler.Context) {
+      func(element *colly.HTMLElement, ctx *Crawler.Context) {
          ctx.Authors = append(ctx.Authors, strings.TrimSpace(element.Text))
       })
 
    // 获取 Content（情况一）
-   w.OnHTML("#group-content-container .field-items > div", 
-      func(element *colly.HTMLElement, ctx *megaCrawler.Context) {
+   w.OnHTML("#group-content-container .field-items> div", 
+      func(element *colly.HTMLElement, ctx *Crawler.Context) {
          ctx.Content = element.Text
       })
 
    // 获取 Content（情况二）
-   w.OnHTML(".group-content .field-items > div", 
-      func(element *colly.HTMLElement, ctx *megaCrawler.Context) {
+   w.OnHTML(".group-content .field-items> div", 
+      func(element *colly.HTMLElement, ctx *Crawler.Context) {
          ctx.Content = element.Text
       })
 
    // 获取 Related Documents 中的 File（/case-docket）
    w.OnHTML(".file > a", 
-      func(element *colly.HTMLElement, ctx *megaCrawler.Context) {
+      func(element *colly.HTMLElement, ctx *Crawler.Context) {
          ctx.File = append(ctx.File, strings.TrimSpace(element.Text))
       })
 }
