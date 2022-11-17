3e521d4981e91624da956cbc1e6846d387685aad
dj <945435098@qq.com>
2022-11-17 17:48:06 +0800

    project2049

---
+++ plugins/project2049/project2049.go
@@ -0,0 +1,52 @@
+package project2049
+
+import (
+	"github.com/gocolly/colly/v2"
+	"megaCrawler/Crawler"
+	"strings"
+)
+
+func init() {
+	w := Crawler.Register("project2049", "2049计划研究所", "https://project2049.net/")
+
+	w.SetStartingUrls([]string{"https://project2049.net/category/blog/",
+		"https://project2049.net/category/publications/policy-briefs/",
+		"https://project2049.net/category/publications/occasional-papers/",
+		"https://project2049.net/category/publications/in-the-news/",
+		"https://project2049.net/events"})
+
+	// 尝试寻找下载pdf的按钮，并如果存在则将页面类型转换为报告
+	w.OnHTML("a.button", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+		if strings.Contains(element.Attr("href"), ".pdf") {
+			ctx.File = append(ctx.File, element.Attr("href"))
+			ctx.PageType = Crawler.Report
+		}
+	})
+	// 从翻页器获取链接并访问
+	w.OnHTML("div.pages>a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+		w.Visit(element.Attr("href"), Crawler.Index)
+	})
+	// 从index访问新闻
+	w.OnHTML("a.post-more", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+		w.Visit(element.Attr("href"), Crawler.News)
+	})
+
+	// new.title
+	w.OnHTML("h1.entry-title", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+		ctx.Title = element.Text
+	})
+
+	//new.publish time
+	w.OnHTML("time.entry-date", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+		ctx.PublicationTime = element.Text
+	})
+	// new.author
+	w.OnHTML("span.fn >a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+		ctx.Authors = append(ctx.Authors, element.Text)
+	})
+	// new .content
+	w.OnHTML("div.the_content_wrapper", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+		ctx.Content = element.Text
+	})
+
+}
