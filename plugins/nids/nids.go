ad3a22d9c62e2e2e46643f664fa708482e870248
Stardust <therulerofstar@gmail.com>
2022-11-18 14:46:45 +0800

    nids

--- plugins/nids/nids.go
+++ plugins/nids/nids.go
@@ -11,39 +11,8 @@ func init() {
 	w.SetStartingUrls([]string{"http://www.nids.mod.go.jp/research/profile/index.html",
 		"http://www.nids.mod.go.jp/publication/index.html"})
 
-	// 尝试寻找下载pdf的按钮，并如果存在则将页面类型转换为报告
-	w.OnHTML("li > a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
-		if strings.Contains(element.Attr("href"), ".pdf") {
-			ctx.File = append(ctx.File, element.Attr("href"))
-			ctx.PageType = Crawler.Report
-		}
-	})
-	w.OnHTML("div > a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
-		if strings.Contains(element.Attr("href"), ".pdf") {
-			ctx.File = append(ctx.File, element.Attr("href"))
-			ctx.PageType = Crawler.Report
-		}
-	})
-	w.OnHTML("p > a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
-		if strings.Contains(element.Attr("href"), ".pdf") {
-			ctx.File = append(ctx.File, element.Attr("href"))
-			ctx.PageType = Crawler.Report
-		}
-	})
-
 	//index
 	w.OnHTML("#profile > ul > li > a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
-		w.Visit(element.Attr("href"), Crawler.Index)
-	})
-
-	//index(report)
-	w.OnHTML("#localnavi > ul > li> a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
-		w.Visit(element.Attr("href"), Crawler.Index)
-	})
-	w.OnHTML("#content > div.section > ul > li> a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
-		w.Visit(element.Attr("href"), Crawler.Index)
-	})
-	w.OnHTML("#content > div.section.mtx > div > div > ul > li> a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
 		w.Visit(element.Attr("href"), Crawler.Index)
 	})
 
@@ -67,4 +36,34 @@ func init() {
 		ctx.Description = element.Text
 	})
 
+	//index(report)
+	w.OnHTML("#localnavi > ul > li> a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+		w.Visit(element.Attr("href"), Crawler.Index)
+	})
+	w.OnHTML("#content > div.section > ul > li> a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+		w.Visit(element.Attr("href"), Crawler.Index)
+	})
+	w.OnHTML("#content > div.section.mtx > div > div > ul > li> a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+		w.Visit(element.Attr("href"), Crawler.Index)
+	})
+
+	// 尝试寻找下载pdf的按钮，并如果存在则将页面类型转换为报告
+	w.OnHTML("li > a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+		if strings.Contains(element.Attr("href"), ".pdf") && (ctx.PageType != Crawler.Expert) {
+			ctx.File = append(ctx.File, element.Attr("href"))
+			ctx.PageType = Crawler.Report
+		}
+	})
+	w.OnHTML("div > a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+		if strings.Contains(element.Attr("href"), ".pdf") && (ctx.PageType != Crawler.Expert) {
+			ctx.File = append(ctx.File, element.Attr("href"))
+			ctx.PageType = Crawler.Report
+		}
+	})
+	w.OnHTML("p > a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+		if strings.Contains(element.Attr("href"), ".pdf") && (ctx.PageType != Crawler.Expert) {
+			ctx.File = append(ctx.File, element.Attr("href"))
+			ctx.PageType = Crawler.Report
+		}
+	})
 }
