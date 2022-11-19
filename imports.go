012bee49507c9c35fe530d12e323c1209e1da39d
Mamoruque <wongchunyau@stu2022.jnu.edu.cn>
2022-11-19 12:18:26 +0800

    Add plugin for www.swp-berlin.org

--- imports.go
+++ imports.go
@@ -31,5 +31,5 @@ import (
 	//_ "megaCrawler/plugins/kdi"
 	//_ "megaCrawler/plugins/asaninst"
 	//_ "megaCrawler/plugins/lowyinstitute"
-	_ "megaCrawler/plugins/rsis"
+	_ "megaCrawler/plugins/swp_berlin"
 )
