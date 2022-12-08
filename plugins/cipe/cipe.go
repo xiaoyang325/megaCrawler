d60dcdee57f60d9c29ea64cb150808e2d43c9c3e
Fox_white <vct.xie@gmail.com>
2022-12-08 22:25:31 +0800

    Fix: dot for class

--- plugins/cipe/cipe.go
+++ plugins/cipe/cipe.go
@@ -8,10 +8,12 @@ import (
 
 func init() {
 	w := Crawler.Register("cipe", "国际私营企业中心", "https://www.cipe.org/")
-	w.SetStartingUrls([]string{"https://www.cipe.org/who-we-are/leadership/",
+	w.SetStartingUrls([]string{
+		"https://www.cipe.org/who-we-are/leadership/",
 		"https://www.cipe.org/who-we-are/board/",
 		"https://www.cipe.org/resources/",
-		"https://www.cipe.org/blog/"})
+		"https://www.cipe.org/blog/",
+	})
 
 	//访问专家
 	w.OnHTML(".people--grid-teaser > a", func(element *colly.HTMLElement, ctx *Crawler.Context) {
@@ -19,17 +21,17 @@ func init() {
 	})
 
 	//专家姓名
-	w.OnHTML("page__title", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+	w.OnHTML(".page__title", func(element *colly.HTMLElement, ctx *Crawler.Context) {
 		ctx.Name = element.Text
 	})
 
 	//专家头衔
-	w.OnHTML("page__subtitle", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+	w.OnHTML(".page__subtitle", func(element *colly.HTMLElement, ctx *Crawler.Context) {
 		ctx.Title = element.Text
 	})
 
 	//专家描述,新闻正文
-	w.OnHTML("field--body", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+	w.OnHTML(".field--body", func(element *colly.HTMLElement, ctx *Crawler.Context) {
 		if ctx.PageType == Crawler.Expert {
 			ctx.Description = element.Text
 		} else if ctx.PageType == Crawler.News || ctx.PageType == Crawler.Report {
@@ -48,7 +50,7 @@ func init() {
 	})
 
 	// 尝试寻找下载pdf的按钮，并如果存在则将页面类型转换为报告
-	w.OnHTML("download-button", func(element *colly.HTMLElement, ctx *Crawler.Context) {
+	w.OnHTML(".download-button", func(element *colly.HTMLElement, ctx *Crawler.Context) {
 		if strings.Contains(element.Attr("href"), ".pdf") {
 			ctx.File = append(ctx.File, element.Attr("href"))
 			ctx.PageType = Crawler.Report
