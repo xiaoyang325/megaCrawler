e4f295a0776b3d4534160d673c141ddf4e3a7f22
Fox_white <vct.xie@gmail.com>
2024-02-20 05:06:39 +0800

    feat: more rules for time cleanup

--- crawlers/cleanup.go
+++ crawlers/cleanup.go
@@ -11,14 +11,30 @@ var replacer = strings.NewReplacer(
 	"Reuters/", "",
 	"Entertainment Desk", "",
 	"REUTERS", "",
+	"Posted on", "",
+	"Published:", "",
+	"am", "AM",
+	"pm", "PM",
 )
+
+var template = []string{
+	"03:04 PM Jan 2, 2006",
+}
 
 func TimeCleanup(timeStr string) time.Time {
 	timeStr = replacer.Replace(timeStr)
 	timeStr = strings.TrimSpace(timeStr)
-	parseAny, err := dateparse.ParseAny(timeStr)
-	if err == nil {
-		return parseAny
+	for _, subStr := range strings.Split(timeStr, "\n") {
+		parse, err := dateparse.ParseAny(subStr)
+		if err == nil {
+			return parse
+		}
+		for _, temp := range template {
+			parse, err = time.Parse(temp, subStr)
+			if err == nil {
+				return parse
+			}
+		}
 	}
 	return time.Time{}
 }
