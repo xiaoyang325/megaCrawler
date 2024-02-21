ae628ca4395fac1eac3a96ec343787fd190e3afa
Fox_white <vct.xie@gmail.com>
2024-02-21 12:21:33 +0800

    fix: remove debug print

--- crawlers/cleanup.go
+++ crawlers/cleanup.go
@@ -30,12 +30,13 @@ var replacer = strings.NewReplacer(
 
 var template = []string{
 	"03:04 PM Jan 2, 2006",
-	"15:04 02/01/2006",
+	"15:04 02 01 2006",
 	"Monday January 2 2006",
 	"03:04 PM EDT Mon January 2 2006",
 	"Monday January 2 2006 03:04 PM IST",
 	"2 01 2006",
 	"02 01 2006 03:04 PM",
+	"02 01 2006 15:04",
 	"2006 01 02 15:04",
 }
 
@@ -44,7 +45,6 @@ func TimeCleanup(timeStr string) time.Time {
 	timeStr = strings.TrimSpace(timeStr)
 	for _, subStr := range strings.Split(timeStr, "\n") {
 		subStr = StandardizeSpaces(strings.TrimSpace(subStr))
-		println(subStr)
 		parse, err := dateparse.ParseAny(subStr)
 		if err == nil {
 			return parse
